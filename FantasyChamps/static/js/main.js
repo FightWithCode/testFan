1155c0613684223e6da3bc2a0872943d86e294bd
FightWithCode <rahulsharmanoj@gmail.com>
2018-06-21 22:16:55 +0530

    0Added More GUI

--- FantasyChamps/static/js/main.js
+++ FantasyChamps/static/js/main.js
@@ -1,3 +1,67 @@
+var deadlineOne = new Date("June 15, 2018 15:00:00").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var t = deadlineOne - now;
+var days = Math.floor(t / (1000 * 60 * 60 * 24));
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+document.getElementById("matchTimeOne").innerHTML = days + " : "
++ hours + " : " + minutes + " : " + seconds;
+    if (t < 0) {
+        clearInterval(x);
+        document.getElementById("matchTimeOne").innerHTML = "EXPIRED";
+    }
+}, 1000);
+var deadlineTwo = new Date("June 16, 2018 15:00:00").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var t = deadlineTwo - now;
+var days = Math.floor(t / (1000 * 60 * 60 * 24));
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+document.getElementById("matchTimeTwo").innerHTML = days + " : "
++ hours + " : " + minutes + " : " + seconds;
+    if (t < 0) {
+        clearInterval(x);
+        document.getElementById("matchTimeTwo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+var deadlineThree = new Date("June 17, 2018 15:00:00").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var t = deadlineThree - now;
+var days = Math.floor(t / (1000 * 60 * 60 * 24));
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+document.getElementById("matchTimeThree").innerHTML = days + " : "
++ hours + " : " + minutes + " : " + seconds;
+    if (t < 0) {
+        clearInterval(x);
+        document.getElementById("matchTimeThree").innerHTML = "EXPIRED";
+    }
+}, 1000);
+var deadlineFour = new Date("June 18, 2018 15:00:00").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var t = deadlineFour - now;
+var days = Math.floor(t / (1000 * 60 * 60 * 24));
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+document.getElementById("matchTimeFour").innerHTML = days + " : "
++ hours + " : " + minutes + " : " + seconds;
+    if (t < 0) {
+        clearInterval(x);
+        document.getElementById("matchTimeFour").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+
+
+
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $(".innerA").on('click', function(event) {
@@ -71,65 +135,4 @@ AOS.init({
 //     <h3>No Need of PAN Card Details<h3>
 //   </div>
 // </div>
-/*   Counter Script
-var deadlineOne = new Date("April 15, 2018 15:00:00").getTime();
-var x = setInterval(function() {
-var now = new Date().getTime();
-var t = deadlineOne - now;
-var days = Math.floor(t / (1000 * 60 * 60 * 24));
-var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((t % (1000 * 60)) / 1000);
-document.getElementById("matchTimeOne").innerHTML = days + " Days "
-+ hours + " Hrs " + minutes + " Mins " + seconds + " Secs ";
-    if (t < 0) {
-        clearInterval(x);
-        document.getElementById("matchTimeOne").innerHTML = "EXPIRED";
-    }
-}, 1000);
-var deadlineTwo = new Date("April 16, 2018 15:00:00").getTime();
-var x = setInterval(function() {
-var now = new Date().getTime();
-var t = deadlineTwo - now;
-var days = Math.floor(t / (1000 * 60 * 60 * 24));
-var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((t % (1000 * 60)) / 1000);
-document.getElementById("matchTimeTwo").innerHTML = days + " Days "
-+ hours + " Hrs " + minutes + " Mins " + seconds + " Secs ";
-    if (t < 0) {
-        clearInterval(x);
-        document.getElementById("matchTimeTwo").innerHTML = "EXPIRED";
-    }
-}, 1000);
-var deadlineThree = new Date("April 17, 2018 15:00:00").getTime();
-var x = setInterval(function() {
-var now = new Date().getTime();
-var t = deadlineThree - now;
-var days = Math.floor(t / (1000 * 60 * 60 * 24));
-var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((t % (1000 * 60)) / 1000);
-document.getElementById("matchTimeThree").innerHTML = days + " Days "
-+ hours + " Hrs " + minutes + " Mins " + seconds + " Secs ";
-    if (t < 0) {
-        clearInterval(x);
-        document.getElementById("matchTimeThree").innerHTML = "EXPIRED";
-    }
-}, 1000);
-var deadlineFour = new Date("April 18, 2018 15:00:00").getTime();
-var x = setInterval(function() {
-var now = new Date().getTime();
-var t = deadlineFour - now;
-var days = Math.floor(t / (1000 * 60 * 60 * 24));
-var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((t % (1000 * 60)) / 1000);
-document.getElementById("matchTimeFour").innerHTML = days + " Days "
-+ hours + " Hrs " + minutes + " Mins " + seconds + " Secs ";
-    if (t < 0) {
-        clearInterval(x);
-        document.getElementById("matchTimeFour").innerHTML = "EXPIRED";
-    }
-}, 1000);
-*/
+//   Counter Script
